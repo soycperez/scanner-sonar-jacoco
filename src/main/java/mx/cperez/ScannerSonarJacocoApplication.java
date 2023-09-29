a24b846d6d81394c93a16239264dea58d7a13455
CESAR PEREZ <soycesar20k18@gmail.com>
2023-09-29 11:09:48 -0600

    firts commit

---
+++ src/main/java/mx/cperez/ScannerSonarJacocoApplication.java
@@ -0,0 +1,13 @@
+package mx.cperez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScannerSonarJacocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScannerSonarJacocoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/cperez/ScannerSonarJacocoApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.cperez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScannerSonarJacocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
